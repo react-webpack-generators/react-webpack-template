d94724832eb855e20ccfa5bfd47a0710b9a1826c
Chris <cs@weblogixx.de>
2015-09-10 14:53:32 +0200

    Added yeoman image as default view.

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,14 @@ require('styles/App.css');
 
 import React from 'react/addons';
 
+let yeomanImage = require('../images/yeoman.png');
+
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        Content
+        <img src={yeomanImage} alt="Yeoman Generator" />
+        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
     );
   }
