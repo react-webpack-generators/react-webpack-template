c2b2d99f61c4012471dcae7ad92214dbc8ce6aaa
Chris <cs@weblogixx.de>
2015-09-16 10:27:22 +0200

    Added support for alternate loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,22 @@ var config = {
         loader: 'style!css'
       },
       {
+        test: /\.sass/,
+        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+      },
+      {
+        test: /\.scss/,
+        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+      },
+      {
+        test: /\.less/,
+        loader: 'style-loader!css-loader!less-loader'
+      },
+      {
+        test: /\.styl/,
+        loader: 'style-loader!css-loader!stylus-loader'
+      },
+      {
         test: /\.(png|jpg|gif|woff|woff2)$/,
         loader: 'url-loader?limit=8192'
       }
