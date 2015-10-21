448ad72b8340336f924b2ab43bcc34f0e7a1e270
sthzg <sthzg@gmx.net>
2015-10-21 14:06:38 +0200

    #4 -- added DefinePlugin to plugins for dist builds

--- cfg/dist.js
+++ cfg/dist.js
@@ -10,6 +10,9 @@ var config = _.merge({
   devtool: 'sourcemap',
   plugins: [
     new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
