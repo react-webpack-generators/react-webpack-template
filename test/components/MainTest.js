834fe2569dec7e1571e03a31f352e03ef0d36c0f
Chris <cs@weblogixx.de>
2016-01-13 07:50:37 +0100

    Updated lodash to current stable, cleaned up unit test formatting

--- test/components/MainTest.js
+++ test/components/MainTest.js
@@ -11,13 +11,13 @@ import createComponent from 'helpers/shallowRenderHelper';
 import Main from 'components/Main';
 
 describe('MainComponent', () => {
-    let MainComponent;
+  let MainComponent;
 
-    beforeEach(() => {
-      MainComponent = createComponent(Main);
-    });
+  beforeEach(() => {
+    MainComponent = createComponent(Main);
+  });
 
-    it('should have its component name as default className', () => {
-      expect(MainComponent.props.className).to.equal('index');
-    });
+  it('should have its component name as default className', () => {
+    expect(MainComponent.props.className).to.equal('index');
+  });
 });
