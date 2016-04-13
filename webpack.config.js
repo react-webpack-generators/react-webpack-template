04d0489bb94459b69900b8720ade8ae914b89bd8
Carlos Corvaia <znja@users.noreply.github.com>
2016-04-13 11:23:08 -0400

    Lingering var in config file
    There was a lingering `var` while throughout the webpack.config.js you use es2015 syntax.

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const args = require('minimist')(process.argv.slice(2));
 const allowedEnvs = ['dev', 'dist', 'test'];
 
 // Set the correct environment
-var env;
+let env;
 if (args._.length > 0 && args._.indexOf('start') !== -1) {
   env = 'test';
 } else if (args.env) {
