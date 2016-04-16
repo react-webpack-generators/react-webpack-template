54ee73f2b67bcac7326455ff54c8e7fccd632903
Chris <cs@weblogixx.de>
2016-04-16 02:13:38 +0200

    Added very basic webpack config

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const yeomanImage = require('../images/yeoman.png');
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="index">
+        <img src={yeomanImage} alt="Yeoman Generator" />
+        <div className="notice">
+          Please edit <code>src/components/Main.js</code> to get started!
+        </div>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+// const webpack = require('webpack');
+const path = require('path');
+
+const x = {
+  context: path.resolve('./src'),
+  debug: true,
+  devtool: 'eval',
+  entry: './index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      },
+      {
+        test: /\.(png|jpg|gif|mp4|ogg|svg|woff|woff2)$/,
+        loaders: ['file-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve('./dist/assets'),
+    filename: 'app.js',
+    publicPath: './assets/'
+  },
+  plugins: [],
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    modules: [
+      path.resolve('./src'),
+      'node_modules'
+    ]
+  }
+};
+
+module.exports = x;
