646a24e6662aefc9c11c05ae7cd4226f1636ce53
Chris <cs@weblogixx.de>
2016-05-19 15:58:31 +0200

    Added backup compat for 1.x (styles global folder)

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class AppComponent extends React.Component {
 
   render() {
     return (
-      <div className="index" styleName="index" onClick={this.handleClick.bind(this)}>
+      <div className="index" styleName="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">
           Please edit <code>src/components/App.js</code> to get started!
