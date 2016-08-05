d88354dff0d10456890dfc758a9bfb9f8aac986e
sthzg <sthzgvie@gmail.com>
2016-08-05 10:55:49 +0200

    Removes dependency to cssmodules - #43
    As the V4 generator supports projects with and without cssmodules, the
    template repo may not make assumptions about them being enabled.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import cssmodules from 'react-css-modules';
-import styles from './app.cssmodule.css';
+import './app.css';
 
 const yeomanImage = require('../images/yeoman.png');
 
-@cssmodules(styles)
 class AppComponent extends React.Component {
 
   render() {
     return (
-      <div className="index" styleName="index">
+      <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">
           Please edit <code>src/components/App.js</code> to get started!
