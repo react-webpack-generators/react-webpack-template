d9ba3b20a815b931ead76eebdafebcd7f27e2000
Liron Boberman <lironboberman@gmail.com>
2016-08-30 16:09:16 +0300

    Added icon generator

---
+++ src/icons/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  files: [
+    './*.svg'
+  ],
+  fontName: 'svgIcons',
+  classPrefix: 'icon-',
+  baseClass: 'Icons',
+  fixedWidth: true,
+  types: ['eot', 'woff', 'ttf', 'svg']
+};
